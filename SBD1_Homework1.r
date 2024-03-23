0a31763411641ab37ae6870be557a382c01a21e4
Larissa Pagliarin <pagll1@bfh.ch>
2024-03-23 16:40:46 +0100

    Erstellt mit Colaboratory

---
+++ SBD1_Homework1.r
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNC3qRYG2uRCiRBH/E/UVTV",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "ir",
+      "display_name": "R"
+    },
+    "language_info": {
+      "name": "R"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/LarissaMargherita/SBD2_Semesterarbeit/blob/master/SBD1_Homework1.r\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "Fa0elUWI5Z5F"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "QObBqUxd8xol"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
